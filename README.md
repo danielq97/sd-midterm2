d3d8764539cc9b2fe6b981dfea17afab69617bd1
mishale22 <sami_na11@hotmail.com>
2019-11-08 11:39:56 -0500

    created 2 endpoints and conection to database

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Distributed Systems exam 2
+
+Adelantos:
+Conexión a base de datos y los 2 endpoints. 
+
+Falta:
+Pruebas
+OpenAPI
---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const MongoClient = require("mongodb").MongoClient;
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+var database, collection;
+
+app.get('/', (req, res, next) => {
+    res.json({ message: 'Welcome to our app :)'});
+  });
+
+app.post('/users', (req,res) =>{
+
+    collection.findOne({ "idnumber": parseInt(req.body.idnumber) }, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+
+        if(result){
+            res.json({ message: 'A user has been created with that number'});
+        }else{
+            collection.insertOne(req.body, (err, result) => {
+                if(err) {
+                    return res.status(500).send(error);
+                }
+                res.json({message:'A new user has been created', user: result.ops});
+            });
+        }
+    });
+});
+
+app.get('/users', (req,res) =>{
+    collection.find({}).toArray((error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        res.json({message:'A list of all users', users: result});
+    });
+});
+
+app.get("*", (req, res) => res.redirect("/"));
+
+app.listen(4000, function () {
+    console.log('Server is running.. on Port 4000');    
+    MongoClient.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-ayp0h.mongodb.net/test?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true},(error, client) => {
+        if(error) {
+            throw error;
+        }
+        database = client.db(process.env.MONGO_DB);
+        collection = database.collection(process.env.MONGO_COLLECTION);
+        console.log("Connected to `" + database + "`!");
+    });
+});
