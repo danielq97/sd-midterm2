1a10a1ec9ca4665d4dae713f37d51c720a48aa6c
mishale22 <sami_na11@hotmail.com>
2019-11-11 17:15:18 -0500

    tests for post endpoint

---
+++ test/TestEndpoints.test.js
@@ -0,0 +1,39 @@
+const agent = require('superagent');
+const statusCode = require('http-status-codes');
+const chai = require('chai');
+
+const { expect } = chai;
+
+describe('Api Tests for Users endpoints', () => {
+
+    it('POST service', async () => {
+      const requestBody = {
+        name: "Sandra",
+        lastname: "Nino",
+        idnumber: 1143874326
+      };
+  
+      const response = await agent.post('http://localhost:4000/users').send(requestBody);
+  
+      expect(response.status).to.equal(statusCode.CREATED);
+      expect(response.body.message).to.eql('A new user has been created');
+      expect(response.body.user[0].name).to.eql(requestBody.name);
+      expect(response.body.user[0].lastname).to.eql(requestBody.lastname);
+      expect(response.body.user[0].idnumber).to.eql(requestBody.idnumber);
+    });
+
+    it('POST service with a user already created', async () => {
+        const requestBody = {
+          name: "Sandra",
+          lastname: "Nino",
+          idnumber: 1143874326
+        };
+            
+        agent.post('http://localhost:4000/users').send(requestBody).then().catch(
+            (response)=> {
+                expect(response.status).to.equal(statusCode.BAD_REQUEST);
+            });
+  
+       
+    });
+});
